90db9c2fb07b6626586c7ab7ebaf9b653b763fb8
Henry Turner <henry.turner@unboxed.co>
2019-02-20 16:49:59 +0000

    Expose loadMap module to call once Google's lib has loaded

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,13 @@
 const { environment } = require('@rails/webpacker')
 
+environment.loaders.append('expose', {
+  test: require.resolve('../../app/javascript/map'),
+  use: [
+    {
+      loader: 'expose-loader',
+      options: 'loadMap'
+    }
+  ]
+})
+
 module.exports = environment
